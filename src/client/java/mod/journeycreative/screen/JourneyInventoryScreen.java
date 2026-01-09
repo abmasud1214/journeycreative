b1bb45cc78428259acd5b43a58db913525c72a20
abmasud1214 <abmasud1214@gmail.com>
2026-01-08 19:04:25 -0700

    Fixed the journey inventory dropping item bug.

--- src/client/java/mod/journeycreative/screen/JourneyInventoryScreen.java
+++ src/client/java/mod/journeycreative/screen/JourneyInventoryScreen.java
@@ -839,7 +839,7 @@ public class JourneyInventoryScreen extends HandledScreen<JourneyInventoryScreen
         }
     }
 
-    protected boolean isClickOutsideBounds(double mouseX, double mouseY, int left, int top, int button) {
+    protected boolean isClickOutsideBounds(double mouseX, double mouseY, int left, int top) {
         boolean bl = mouseX < (double)left || mouseY < (double)top || mouseX >= (double)(left + this.backgroundWidth) || mouseY >= (double)(top + this.backgroundHeight);
         this.lastClickOutsideBounds = bl && !this.isClickInTab(selectedTab, mouseX, mouseY);
         return this.lastClickOutsideBounds;
